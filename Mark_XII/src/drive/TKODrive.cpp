b46b38ed0b6d3affc55566746e5d71bbfbd9c7e8
Vadim Korolik <vkorolik@gmail.com>
2015-01-03 22:07:00 -0800

    Testing Eclipse committing

--- Mark_XII/src/drive/TKODrive.cpp
+++ Mark_XII/src/drive/TKODrive.cpp
@@ -10,11 +10,10 @@
 TKODrive::TKODrive()
 {
 	// TODO Auto-generated constructor stub
-
+	//TODO make sure to use the TKOPointers and to catch the TKOError(s)
 }
 
 TKODrive::~TKODrive()
 {
 	// TODO Auto-generated destructor stub
 }
-

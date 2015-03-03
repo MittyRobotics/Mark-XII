fee7a112f0011f49369de29e2f305233d04e3614
Adam Filiz <adamfiliz16@mittymonarch.com>
2015-03-02 16:59:19 -0800

    Still working. WHERE ARE YOU BOUNDING RECT WIDTH

--- Mark_XII_Java/src/org/usfirst/frc/team1351/robot/vision/TKOVision.java
+++ Mark_XII_Java/src/org/usfirst/frc/team1351/robot/vision/TKOVision.java
@@ -1,5 +1,5 @@
 //Last edited by Adam Filiz
-//on 2/26/15
+//on 3/2/15
 
 package org.usfirst.frc.team1351.robot.vision;
 
@@ -32,8 +32,8 @@ public class TKOVision implements Runnable
 	int session;
     Image frame, BinaryImage, MorphImage, CloseImage, FillImage;
     AxisCamera camera;
-   double distanceToTote, boundingWidthRight, boundingWidthLeft, trueBoundingWidth, midBoundingPoint, finalAngleValue;
-   int boundingWidth;
+   double distanceToTote, boundingWidthRight, boundingWidthLeft, trueBoundingWidth, midBoundingPoint, finalAngleValue, boundingWidth;
+ 
 	
 	protected TKOVision()
 	{
@@ -90,38 +90,42 @@ public class TKOVision implements Runnable
     	
     }
     
-    public Boolean processImage(){ //VADIM I WILL FINISH THIS BY SATURDAY
+    public Boolean processImage()
+    { 
     	
-    	System.out.println("Actually starting processing now!!!"); //declare to start function
+    System.out.println("Actually starting processing now!!!"); //declare to start function
     
-    	camera.writeBrightness(20); //20 for now - will edit later
+    for(double j = 12.; j < distanceToTote; j++){
+    	camera.writeBrightness(20); //20 for now - will test later with camera
     	camera.getImage(frame); //Get image from frame
     	
-    	if(frame == null) {
-    		System.out.println("THERE AIN'T AN IMAGE TO PROCESS");
+    	if(frame == null) { //checks if there is an image
+    		System.out.println("There is not an image to process!");
     		return false;
     	}
     	
-    	System.out.println("THERE IS AN IMAGE TO PROCESS");
+    	System.out.println("There is an image to process!");
     	
-    	if(camera.getImage(frame) == false) { //sort of at an impass here
-    		System.out.println("RAW IMAGE DOESNT WORK BRO");
+    	/*if(camera.getImage(frame) == false) { 
+    		System.out.println("RAW IMAGE DOESNT WORK");
     		}
+    	*/ //Not sure if necessary for code - must test to be sure
     	
-    	System.out.println("RAW IMAGE WORKS");
+    	//System.out.println("RAW IMAGE WORKS");
     	
     	
     	NIVision.imaqColorThreshold(frame, BinaryImage, 0, ColorMode.HSV, new NIVision.Range(100, 140), new NIVision.Range(104, 255), new NIVision.Range(92, 132)); 
-    	System.out.println("Processed binary/color"); //turns raw image into binary image using color threshold
+    	//turns raw image into binary image using color threshold
     	
     	NIVision.imaqMorphology(MorphImage, BinaryImage, NIVision.MorphologyMethod.HITMISS, new StructuringElement()); //Add structuringElement
-    	System.out.println("Removed small objects"); //removes noise and extra particles that might turn up
+    	//removes noise and extra particles that might turn up
     	
     	NIVision.imaqMorphology(CloseImage, MorphImage, NIVision.MorphologyMethod.CLOSE, new StructuringElement());
-    	System.out.println("Closed objects"); //closes the object to make sure it is solid
+    	//closes the object to make sure it is solid (might not be necessary, must test to be sure)
     	
     	NIVision.imaqFillHoles(FillImage, CloseImage, 4);
-    	System.out.println("Filled rest of objects"); //fills any holes that might still exist
+    	//fills any holes that might still exist
+    	System.out.println("Processed image");
     	
     	MeasurementValue.WIDTH.getValue(); //This is what is supposed to get the bounding box width from FillImage
     	//Still not sure how to use it to get that value, since there is no argument for processing an image
@@ -131,7 +135,7 @@ public class TKOVision implements Runnable
     	
     	
     	
-    if(boundingWidth > 0){
+    if(boundingWidth <= 0){
     		System.out.println("Particle analysis did not return values!");
     		return false;
 
@@ -141,17 +145,18 @@ public class TKOVision implements Runnable
     
     distanceToTote = distance(boundingWidth);
     
-    System.out.println("The current distance to tote is: " + distanceToTote);
+    System.out.println("The current distance to tote is: " + distanceToTote + "inches");
     
-    
+    	}
+    return true;
     
    
     }
     
-    public double distance(int width) {
-    	double dist = 7/(width * 1.324); //TODO: fix distance equation later. Change values and use average bounding box distance
+    public double distance(double width) {
+    	double dist = 4480/(width * .8696); //TODO: fix distance equation later. Change values and use average bounding box distance
     
-    	return dist; //TODO: Make the calculated boundingBox width the average of the two bounding boxes' widths
+    	return dist; //TODO: Make the calculated boundingBox width the average of the two bounding boxes' widths 
    }
     
    public double offAngle(){ //gets the offset angle needed for the robot to turn the right amount of degrees
@@ -179,8 +184,7 @@ public class TKOVision implements Runnable
 					visionThread.wait(5);
 				}
 			}
-		} catch (InterruptedException e)
-		{
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}

45a9a34e3dd9cb31b7c1808fd820d594d3842661
Ben <bumjinkim16@mittymonarch.com>
2015-03-29 16:35:59 -0700

    Rollers controlled independently

--- MarkXII_OfficialCode(Java)/src/org/usfirst/team1351/robot/evom/TKOPneumatics.java
+++ MarkXII_OfficialCode(Java)/src/org/usfirst/team1351/robot/evom/TKOPneumatics.java
@@ -161,8 +161,8 @@ public class TKOPneumatics implements Runnable
 	{
 		try
 		{
-			TKOHardware.getPickupTalon(0).set(TKOHardware.getJoystick(2).getY());
-			TKOHardware.getPickupTalon(1).set(TKOHardware.getJoystick(2).getY());
+			TKOHardware.getPickupTalon(0).set(TKOHardware.getJoystick(2).getY() * 0.5);
+			TKOHardware.getPickupTalon(1).set(-TKOHardware.getJoystick(3).getY() * 0.5);
 		}
 		catch (TKOException e)
 		{
